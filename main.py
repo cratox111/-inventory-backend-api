ec8c2b9f6ad10204d32c4cc2067304b2270482b1
cratox111 <diocgon2011@gmail.com>
2025-12-08 21:44:02 -0600

    Añadi CORS

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from routes import auth, products
 
 app = FastAPI()
 
+CORS = CORSMiddleware()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Routers
 app.include_router(auth.router)
 app.include_router(products.router)
